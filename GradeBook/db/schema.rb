2456b1e1cff01345cfd4d4f3a36bf3720ceed968
melohan <melodie.ohan@cpnv.ch>
2023-03-13 11:53:25 +0100

    Add models and seeder data

--- GradeBook/db/schema.rb
+++ GradeBook/db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema[7.0].define(version: 2023_03_13_095025) do
   create_table "user_types", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "name", limit: 60, null: false
-    t.string "slug", limit: 5, null: false
+    t.string "slug", limit: 10, null: false
     t.string "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
