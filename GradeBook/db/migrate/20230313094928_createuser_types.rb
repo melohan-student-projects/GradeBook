85dd289a18165ca701f6929fb0281d70eadf505b
melohan <melodie.ohan@cpnv.ch>
2023-03-13 10:54:57 +0100

    Add user types migration file

---
+++ GradeBook/db/migrate/20230313094928_createuser_types.rb
@@ -0,0 +1,10 @@
+class CreateuserTypes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_types do |t|
+      t.string :name, limit: 60, null: false
+      t.string :slug, limit: 5, null: false
+      t.string :description, limit: 255, null: false
+      t.timestamps null: false
+    end
+  end
+end
--- GradeBook/db/schema.rb
+++ GradeBook/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_12_204151) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_13_095025) do
+  create_table "user_types", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
+    t.string "name", limit: 60, null: false
+    t.string "slug", limit: 5, null: false
+    t.string "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_general_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -22,8 +30,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_12_204151) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_type_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_type_id"], name: "index_users_on_user_type_id"
   end
 
+  add_foreign_key "users", "user_types"
 end
