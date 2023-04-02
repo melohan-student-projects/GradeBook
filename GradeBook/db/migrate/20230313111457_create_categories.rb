50619a2d76775cc54e30270d6527fff9ac2119ca
melohan <melodie.ohan@cpnv.ch>
2023-04-02 14:58:10 +0200

    Create lectures seeder and migration file

--- GradeBook/db/migrate/20230313111457_create_categories.rb
+++ GradeBook/db/migrate/20230313111457_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration[7.0]
   def change
     create_table :categories do |t|
-      t.string :name, limit: 60, null: false
+      t.string :name, limit: 255, null: false
 
       t.timestamps
     end
